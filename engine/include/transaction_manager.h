104ce6deb06d76801a37b88b0c45af16c413b4bf
Somya Kumar <33091016+Saumya837@users.noreply.github.com>
2026-04-26 10:36:28 +0530

    feat: implement TransactionManager
    begin/commit/abort with XID allocation and active set tracking.
    get_snapshot captures xmin/xmax/active_xids at point in time.
    Constructor initializes next_xid=3, matching Postgres XID_FROZEN convention.

--- engine/include/transaction_manager.h
+++ engine/include/transaction_manager.h
@@ -19,10 +19,11 @@ namespace minidb{
             std::unordered_set<TransactionId> active_xids_;              // Set od active transactions
             std::unordered_map<TransactionId, XactStatus> xact_status_;  // Transaction status map
         public :
+            TransactionManager() : next_xid_(3) {}
             TransactionId begin();
             void commit_transaction(TransactionId xid);
             void abort_transaction(TransactionId xid);
-            Snapshot get_snapshot(TransactionId current_xid);
+            Snapshot get_snapshot(TransactionId current_xid, CommandId current_cid);
             XactStatus get_transaction_status(TransactionId xid);
     };
 };
---
+++ engine/src/transaction_manager.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include "transaction_manager.h"
+#include "snapshot.h"
+
+namespace minidb{
+
+    TransactionId TransactionManager::begin(){
+        TransactionId xid = next_xid_++;
+        active_xids_.insert(xid);
+        xact_status_[xid] = XactStatus::Active;
+        return xid;
+    }
+
+    void TransactionManager::commit_transaction(TransactionId xid){
+        xact_status_[xid] = XactStatus::Commited;
+        active_xids_.erase(xid);
+    }
+
+
+    void TransactionManager::abort_transaction(TransactionId xid){
+        xact_status_[xid] = XactStatus::Aborted;
+        active_xids_.erase(xid);
+    }
+
+    Snapshot TransactionManager::get_snapshot(TransactionId current_xid, CommandId current_cid){
+        Snapshot snap = Snapshot();
+        if(active_xids_.empty()){
+            snap.xmin = next_xid_;
+            snap.xmax = next_xid_;
+        }
+        else{
+            auto min = std::min_element(active_xids_.begin(), active_xids_.end());
+            snap.xmin = *min;
+            snap.xmax = next_xid_;
+        }
+        snap.current_xid = current_xid;
+        snap.current_cid = current_cid;
+        snap.active_xids = active_xids_;
+        return snap;
+    }
+
+    XactStatus TransactionManager::get_transaction_status(TransactionId xid){
+        return xact_status_.at(xid);
+    }
+}
